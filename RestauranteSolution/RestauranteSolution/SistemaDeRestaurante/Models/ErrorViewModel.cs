a200da0a7b1c74216a5e4b95b6d343e092d63112
Cássio Souza <cassiovenanciosouza@gmail.com>
2024-02-22 09:27:37 -0300

    Delete RestauranteSolution/RestauranteSolution directory

--- RestauranteSolution/RestauranteSolution/SistemaDeRestaurante/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaDeRestaurante.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
