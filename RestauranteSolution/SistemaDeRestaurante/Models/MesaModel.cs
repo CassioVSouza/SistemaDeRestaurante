c402969ae03375237af8aa50ac081fce4da9f711
Cássio Souza <cassiovenanciosouza@gmail.com>
2024-02-22 09:30:51 -0300

    new files

---
+++ RestauranteSolution/SistemaDeRestaurante/Models/MesaModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaDeRestaurante.Models
+{
+    public class MesaModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public List<PedidosModel> Pedidos { get; set; } = null!;
+        public int IDPedidos { get; set; }
+        public int Sessao { get; set; }
+    }
+}
---
+++ RestauranteSolution/SistemaDeRestaurante/Models/ProdutoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaDeRestaurante.Models
+{
+    public class ProdutoModel
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; } = null!;
+        public double Preco { get; set; }
+    }
+}
